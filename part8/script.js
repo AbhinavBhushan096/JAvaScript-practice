cc53030a540cb9ca12b68bc48c4d66226052a8c2
Abhinav Bhushan <abhinavbhushan47@gmail.com>
2025-01-21 21:22:44 +0530

    second commit-part8 mainly edited

--- part8/script.js
+++ part8/script.js
@@ -2,10 +2,67 @@
 //paragraph or event // event fiirst most of the time
 
 // document.querySelector('.')
+
+const originalText = document.getElementById("myParagraph").textContent;
+let paragraph = document.getElementById("myParagraph");
+let isChanged = false;
 document
   .getElementById("changeTextButton")
   .addEventListener("click", function () {
-    //   console.log(this);
-    let paragraph = document.getElementById("myParagraph");
     console.log(paragraph);
-  }); //learn in mdn for detail
+    if (isChanged) {
+      paragraph.textContent = originalText;
+    } else {
+      paragraph.textContent = "the paragraph is changed";
+    }
+    isChanged = !isChanged;
+  });
+
+// document
+//   .getElementById("changeTextButton")
+//   .addEventListener("dblclick", function () {
+//     console.log("Doube-click detected");
+//     paragraph.textContent = originalText;
+//   });
+
+//example 2
+let ishighlight = false;
+document
+  .getElementById("highlightFirstCity")
+  .addEventListener("click", function () {
+    let hold = document.getElementById("citiesList");
+    // console.log(hold);
+    let fisrtCity = hold.firstElementChild;
+    if (ishighlight) {
+      fisrtCity.classList.remove("highlight");
+    } else {
+      fisrtCity.classList.add("highlight");
+    }
+
+    ishighlight = !ishighlight;
+  });
+
+//exampler-3
+
+document.getElementById("changeOrder").addEventListener("click", function () {
+  let coffeeType = document.getElementById("coffeeOrder");
+  coffeeType.textContent = "Espresso";
+  coffeeType.style.backgroundColor = "red";
+  coffeeType.style.fontStyle = "Bold";
+});
+
+//example - 4
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+  document.getElementById("shoppingList").appendChild(newItem);
+});
+
+
+//example-5
+document.getElementById("removeLastTask").addEventListener("click",function(){
+  let taskitem=document.getElementById("taskList");
+  taskitem.remove()
+});
+
+//example 6
--- rough/script.js
+++ rough/script.js
@@ -3,13 +3,30 @@
 console.log(hold)
 let hold2 = document.getElementById("myParagraph");
 console.log(hold2) */
-document.getElementById("changeTextButton").addEventListener('click',function () {
-    let paragraph = document.getElementById("myParagraph")
-    console.log(paragraph)
+//Example -1
+/* const originalText = document.getElementById("myParagraph").textContent;
+let paragraph = document.getElementById("myParagraph");
+document
+  .getElementById("changeTextButton")
+  .addEventListener("click", function () {
+    console.log(paragraph);
+    paragraph.textContent = "the paragraph is changed";
+  });
+
+document
+  .getElementById("changeTextButton")
+  .addEventListener("dblclick", function () {
+    console.log("Doube-click detected");
+    paragraph.textContent = originalText;
+  });
+ */
+// let arr = [2,4,1,34,34,21,12,4,5,8,7,6]
+
+// arr.sort()// this is wrong as sorting in array is done by strings
+// console.log(arr)
+
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+  document.getElementById("shoppingList").appendChild(newItem);
 });
-
-
-let arr = [2,4,1,34,34,21,12,4,5,8,7,6]
-
-arr.sort()// this is wrong as sorting in array is done by strings
-console.log(arr)
